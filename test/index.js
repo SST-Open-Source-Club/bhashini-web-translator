3362471d56dfc480268c45ab559ae1d52b52c04d
Arth <probablyarth@gmail.com>
2023-11-11 15:00:25 +0530

    remove apiKey and userID

--- test/index.js
+++ test/index.js
@@ -1,7 +1,4 @@
 import { BhashiniTranslator } from "@probablyarth/bhashini";
-const translator = new BhashiniTranslator(
-  "58e362d3f7-f602-4ae6-bb29-5c72883f9a54",
-  "a10ff891057547ba81fc48713426d89b"
-);
+const translator = new BhashiniTranslator("<apiKey>", "<userID>");
 
 translator.translateDOM(document.body, "en", "hi");
