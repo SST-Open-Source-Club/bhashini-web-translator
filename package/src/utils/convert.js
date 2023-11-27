887e6f1b5a468b94021c001ff28762efaf5e2904
Arth <probablyarth@gmail.com>
2023-11-27 15:29:57 +0530

    seperate library for nodejs and web

--- package/src/utils/convert.js
+++ package/src/utils/convert.js
@@ -1,11 +1,4 @@
-import { JSDOM } from "jsdom";
-
 export const htmlStringToDOM = (html) => {
-  const dom = new JSDOM(html);
-  if (dom.window.document.body.textContent === html) {
-    throw new Error(
-      `Error converting HTML string to DOM. Please check if the HTML string is valid.`
-    );
-  }
-  return dom.window.document.body;
+  const parser = new DOMParser();
+  return parser.parseFromString(html, "text/html").body;
 };
