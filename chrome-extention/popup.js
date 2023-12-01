c09577e831c6b3535f2b63f362de9fb727e4ca32
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2023-12-01 19:46:07 +0530

    Changed design of chrome extention

--- chrome-extention/popup.js
+++ chrome-extention/popup.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const translateButton = document.getElementById("translateButton");
 
   translateButton.addEventListener("click", function () {
+    translateButton.textContent = "Translating...";
+    translateButton.disabled = true;
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
         action: "translateContent",
