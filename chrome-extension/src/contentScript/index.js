1cd219919f21292ce8aa79960d7bc7d7e135fdcb
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-02 23:13:52 +0530

    Added reset button, improved UI

--- chrome-extension/src/contentScript/index.js
+++ chrome-extension/src/contentScript/index.js
@@ -1,34 +1,29 @@
 import BhashiniTranslator from '@scaler-school-of-technology/bhashini-web-translator'
 
-const Bhashini = new BhashiniTranslator(
-  '019a562b7f-bb9c-4440-8b79-11b170353130',
-  '48115d2ab7f24c55b8b29af34806050c',
-)
+const apiKey = import.meta.env.VITE_API_KEY
+const userId = import.meta.env.VITE_USER_ID
 
-chrome.runtime.onMessage.addListener( function (request, sender, sendResponse) {
+const Bhashini = new BhashiniTranslator(apiKey, userId)
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === 'translateContent') {
-    translateDom(request).then(
-      (res)=>{
-        if(res){
-          sendResponse(res);
-        }else{
-          sendResponse({msg:"error"})
-        }
-      }
-    );
-    return true;
+    translateDom(request).then(sendResponse)
+    return true
   }
 })
 
 const translateDom = async (request) => {
-  const res = await Bhashini.translateDOM(document.body,request.sourceLanguage,request.targetLanguage,22);
+  const res = await Bhashini.translateDOM(
+    document.body,
+    request.sourceLanguage,
+    request.targetLanguage,
+    22,
+  )
 
-  if(res){
-    console.log("Response from async func")
-    return "success";
-    // sendResponse({msg:"success"});
-  }else{
-    return "error";
-    // sendResponse({msg:"error"});
+  if (res) {
+    console.log('Response from async func')
+    return 'success'
+  } else {
+    return 'error'
   }
 }
