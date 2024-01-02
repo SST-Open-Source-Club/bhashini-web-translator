9aac33dc42ca67bfcdf3f43590f6a6515e323ac2
Abhinav Gupta <Abhinav.gupta@outlook.de>
2024-01-02 23:13:52 +0530

    Removed source button added checkbox

--- chrome-extension/src/background/index.js
+++ chrome-extension/src/background/index.js
@@ -1,7 +1,17 @@
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   if (request.action === 'translateContent') {
-//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//       chrome.tabs.executeScript(tabs[0].id, { file: 'content.js' })
-//     })
-//   }
-// })
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+    chrome.storage.sync.get(['default'], function (result) {
+      if (result.default) {
+        chrome.storage.sync.get(['targetLanguage'], function (result) {
+          if (result.targetLanguage) {
+            chrome.tabs.sendMessage(tabId, {
+              action: 'translateContent',
+              sourceLanguage: 'en',
+              targetLanguage: result.targetLanguage,
+            })
+          }
+        })
+      }
+    })
+  }
+})
--- chrome-extension/src/popup/Popup.jsx
+++ chrome-extension/src/popup/Popup.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './Popup.css'
 
 export const Popup = () => {
-  const [sourceLanguage, setSourceLanguage] = useState('en')
   const [targetLanguage, setTargetLanguage] = useState('hi')
+  const [translating, setTranslating] = useState(false)
+  const [isDefault, setDefault] = useState(false)
 
-  const [translating, setTransalting] = useState(false)
+  const handleDefault = (event) => {
+    setDefault(event.target.checked)
+    chrome.storage.sync.set({ default: event.target.checked }, function () {
+      console.log('Value is of default set to ' + event.target.checked)
+    })
 
-  const handleSourceLanguageChange = (event) => {
-    console.log(event.target.value)
-    setSourceLanguage(event.target.value)
+    if (event.target.checked) {
+      chrome.storage.sync.set({ targetLanguage: targetLanguage }, function () {
+        console.log('Value is target language set to ' + targetLanguage)
+      })
+    }
   }
 
   const handleTargetLanguageChange = (event) => {
@@ -18,55 +25,44 @@ export const Popup = () => {
   }
 
   const handleTranslateClick = () => {
-    setTransalting(true)
+    setTranslating(true)
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(
         tabs[0].id,
         {
           action: 'translateContent',
-          sourceLanguage: sourceLanguage,
+          sourceLanguage: 'en',
           targetLanguage: targetLanguage,
         },
         (response) => {
-          if(response){
-            console.log(response);
+          if (response.message === 'success') {
             setTransalting(false)
           }
-        else{
-          console.log("error");
-        }
-       
         },
       )
     })
   }
 
+  useEffect(() => {
+    chrome.storage.sync.get(['default'], function (result) {
+      if (result.default) {
+        setDefault(result.default)
+      }
+    })
+
+    if (isDefault) {
+      chrome.storage.sync.get(['targetLanguage'], function (result) {
+        if (result.targetLanguage) {
+          setTargetLanguage(result.targetLanguage)
+        }
+      })
+      handleTranslateClick()
+    }
+  }, [isDefault, targetLanguage])
+
   return (
     <div className="language-select-container">
       <img src="img/logo.png" alt="Bhashini" class="logo" />
-
-      <div className="selectCtn">
-        <label>Translate from:</label>
-        <select
-          id="sourceLanguage"
-          value={sourceLanguage}
-          onChange={handleSourceLanguageChange}
-          className="translate-from"
-        >
-          <option value="en">English</option>
-          <option value="hi">Hindi</option>
-          <option value="ta">Tamil</option>
-          <option value="te">Telugu</option>
-          <option value="ml">Malayalam</option>
-          <option value="mr">Marathi</option>
-          <option value="bn">Bengali</option>
-          <option value="as">Assamese</option>
-          <option value="gu">Gujarati</option>
-          <option value="kn">Kannada</option>
-          <option value="or">Odia</option>
-          <option value="pa">Punjabi</option>
-        </select>
-      </div>
 
       <div className="selectCtn">
         <label>Translate to:</label>
@@ -91,20 +87,28 @@ export const Popup = () => {
         </select>
       </div>
 
+      <div className="checkbox-container">
+        <label for="saveAsDefault">
+          <div>Remember for future use.</div>
+          <input type="checkbox" id="saveAsDefault" onChange={handleDefault} checked={isDefault} />
+        </label>
+      </div>
+
       <div className="btn-container">
         {!translating ? (
           <button id="translateButton" onClick={handleTranslateClick}>
             Translate
           </button>
         ) : (
-          <img src="/img/loading.png" alt="translating..." id='loading-img'/>
+          <img src="/img/loading.png" alt="translating..." id="loading-img" />
         )}
       </div>
-      <div className='footer-cont'>
-      <footer className="footer-ctn">
-        <label className="footer">Developed by</label>
-        <img src="img/scaler.png" alt="Bhashini" className="scaler-logo" />
-      </footer>
+
+      <div className="footer-cont">
+        <footer className="footer-ctn">
+          <label className="footer">Developed by</label>
+          <img src="img/scaler.png" alt="Bhashini" className="scaler-logo" />
+        </footer>
       </div>
     </div>
   )
