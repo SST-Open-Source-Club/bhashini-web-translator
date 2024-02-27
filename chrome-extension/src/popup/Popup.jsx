d79f873911e5e626b7aacc970bb244ba869dec5e
Prem B. Shah <techie.prembshah@gmail.com>
2024-02-27 11:38:17 +0530

    Fixed minor bugs in checkbox

--- chrome-extension/src/popup/Popup.jsx
+++ chrome-extension/src/popup/Popup.jsx
@@ -38,14 +38,6 @@ export const Popup = () => {
   },[translating])
 
   const handleTargetLanguageChange = (event) => {
-    if(event.target.value !== 'none') {
-      const errorMessage = document.querySelector('.errorMessage')
-      errorMessage.style.display = 'none'
-      setCheckBoxDisabled(false);
-    }
-    else{
-      setCheckBoxDisabled(true);
-    }
     setTargetLanguage(event.target.value)
     if(event.target.value !== 'none' && !translating) {
       const errorMessage = document.querySelector('.errorMessage')
